c883be9112d5efcf79f937cbd564ec20dc6fdac2
Jacob Taeyoung Yu <muuty.me@gmail.com>
2025-02-22 16:56:59 +1000

    add hdbscan outlier

--- main.py
+++ main.py
@@ -10,9 +10,7 @@ import basicts
 import os
 if __name__ == '__main__':
     for ratio in [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]:
-        for exp_num in range(0,5):
-            if exp_num==0 and ratio < 0.4:
-                continue
+        for exp_num in range(5):
             CFG.DEVICE_NUM = 0
 
             # SELECTION
@@ -21,10 +19,9 @@ if __name__ == '__main__':
             CFG.TRAIN.DATA.EMBEDDING_STRATEGY = None
 
             # OUTLIER
-            CFG.TRAIN.DATA.OUTLIER_REMOVAL_RUNNER = CFG.RUNNER
-            CFG.TRAIN.DATA.OUTLIER_REMOVAL_STRATEGY = 'model_based'
-            CFG.TRAIN.DATA.OUTLIER_REMOVAL_MODEL_PATH = 'checkpoints/STGCNChebGraphConv/PEMS03_100_12_12/k_center_greedy-raw/ration-0.2/0/52c71c8e73ad9255cdcf12d8dafde844/STGCNChebGraphConv_067.pt'
-            CFG.TRAIN.DATA.OUTLIER_REMOVAL_RATIO = 0.1
+            CFG.TRAIN.DATA.OUTLIER_REMOVAL_EMBEDDING_STRATEGY = 'umap'
+            CFG.TRAIN.DATA.OUTLIER_REMOVAL_STRATEGY = 'hdbscan'
+            CFG.TRAIN.DATA.OUTLIER_REMOVAL_RATIO = ratio
             CFG.TRAIN.CKPT_SAVE_DIR = os.path.join(
                 'checkpoints',
                 MODEL_ARCH.__name__,
