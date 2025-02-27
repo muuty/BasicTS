702ee02a9ae1e292b22a28e4b988b6566acd50f3
Jacob Taeyoung Yu <muuty.me@gmail.com>
2025-02-27 13:28:06 +1000

    fix path

--- main.py
+++ main.py
@@ -1,41 +1,35 @@
-from baselines.STGCN.PEMS03 import *
-from datetime import datetime
-from selection.hdbscan_selection import HDBSCANSelection
-from selection.random_selection import RandomSelection
-from selection.k_random_greedy import KCenterGreedySelection
-from selection.embedding.factory import get_embedding
+from easytorch.config import init_cfg
+from basicts.utils import get_regular_settings
 
 
 import basicts
 import os
 if __name__ == '__main__':
-    for ratio in [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]:
-        for exp_num in range(5):
-            CFG.DEVICE_NUM = 0
+    datasets = ('METR-LA', 'PEMS04', 'PEMS07', 'PEMS08', 'PEMS-BAY')
+    for dataset in datasets:
+        for ratio in [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]:
+            for exp_num in range(5):
+                dataset = 'PEMS04'
+                regular_settings = get_regular_settings(dataset)
+                INPUT_LEN = regular_settings['INPUT_LEN']
+                OUTPUT_LEN = regular_settings['OUTPUT_LEN']
 
-            # SELECTION
-            CFG.TRAIN.DATA.SELECTION_RATIO = ratio
-            CFG.TRAIN.DATA.SELECTION_STRATEGY = 'random'
-            CFG.TRAIN.DATA.EMBEDDING_STRATEGY = None
+                CFG = init_cfg(f"baselines/STGCN/{dataset}.py", True)
+                CFG.DEVICE_NUM = 0
 
-            # OUTLIER
-            CFG.TRAIN.DATA.OUTLIER_REMOVAL_EMBEDDING_STRATEGY = 'umap'
-            CFG.TRAIN.DATA.OUTLIER_REMOVAL_STRATEGY = 'hdbscan'
-            CFG.TRAIN.DATA.OUTLIER_REMOVAL_RATIO = ratio
-            CFG.TRAIN.CKPT_SAVE_DIR = os.path.join(
-                'checkpoints',
-                MODEL_ARCH.__name__,
-                '_'.join([DATA_NAME, str(CFG.TRAIN.NUM_EPOCHS), str(INPUT_LEN), str(OUTPUT_LEN)]),
-                f"{CFG.TRAIN.DATA.SELECTION_STRATEGY}-{CFG.TRAIN.DATA.EMBEDDING_STRATEGY}", f"ration-{ratio}", str(exp_num)
-            )
-            basicts.launch_training(CFG, 'mps', node_rank=0)
-    # from baselines.STGCN.PEMS03 import *
-    #
-    # from easytorch.device import set_device_type
-    # from easytorch.config import init_cfg
-    #
-    # CFG = init_cfg(CFG)
-    # set_device_type("mps")
-    # dataset = SimpleTimeSeriesForecastingRunner(CFG).build_train_dataset(CFG)
-    # selection = HDBSCANSelection(dataset, ratio=0.1)
-    # k_greedy_indices = selection.select_indices()
+                # SELECTION
+                CFG.TRAIN.DATA.SELECTION_RATIO = 0.01
+                CFG.TRAIN.DATA.SELECTION_STRATEGY = 'random'
+                CFG.TRAIN.DATA.EMBEDDING_STRATEGY = None
+
+                # # OUTLIER
+                # CFG.TRAIN.DATA.OUTLIER_REMOVAL_EMBEDDING_STRATEGY = 'umap'
+                # CFG.TRAIN.DATA.OUTLIER_REMOVAL_STRATEGY = 'hdbscan'
+                # CFG.TRAIN.DATA.OUTLIER_REMOVAL_RATIO = ratio
+                CFG.TRAIN.CKPT_SAVE_DIR = os.path.join(
+                    'checkpoints',
+                    CFG.MODEL.NAME,
+                    '_'.join([dataset, str(CFG.TRAIN.NUM_EPOCHS), str(INPUT_LEN), str(OUTPUT_LEN)]),
+                    f"{CFG.TRAIN.DATA.SELECTION_STRATEGY}",f"ratio-{CFG.TRAIN.DATA.SELECTION_RATIO}", str(exp_num)
+                )
+                basicts.launch_training(CFG, 'mps', node_rank=0)
